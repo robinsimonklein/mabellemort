4f68279b089e7f2a2bcf13bb03b52466a497f8ef
Robin Simonklein <rsimonklein@gmail.com>
2019-05-13 15:46:41 +0200

    Reinstall Vuex

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
+import Vuex from 'vuex'
 import App from './App.vue'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   render: h => h(App),
