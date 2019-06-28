181205bb31635e1ef5acbc6c8c6e141283386037
Robin Simonklein <rsimonklein@gmail.com>
2019-06-28 04:01:31 +0200

    Add apiai and uuid in FreeResponse

--- src/components/interactions/FreeResponse.vue
+++ src/components/interactions/FreeResponse.vue
@@ -1,22 +1,24 @@
 <template>
     <div class="free-response">
-        <form class="free-response__form">
-            <input type="text" class="free-response__input" name="response" />
-            <input type="submit" name="send" value="Envoyer"/>
+        <form @submit.prevent="sendDialogflow" class="free-response__form" novalidate="true">
+            <input type="text" class="free-response__input" name="response" v-model="response" id="response" placeholder="Message..." autocomplete="off"/>
+            <input type="submit" class="free-response__submit" name="send" value="Envoyer"/>
         </form>
     </div>
 </template>
 
 <script>
+    import uuidv1 from 'uuid/v1'
+
     export default {
         name: "FreeResponse",
         props: {
-            data: {
-
-            },
-            fluid: {
-                type: Boolean,
-                default: false
+        },
+        data() {
+            return {
+                response: null,
+                ACCESS_TOKEN: 'de5f56a7d3ea4b63b3033f5e452209d5',
+                AI_SESSION_ID: null
             }
         },
         methods: {
@@ -26,12 +28,16 @@
                     top: document.querySelector('.messages-container').scrollHeight + 30,
                     behavior: 'smooth'}
                 );
+            },
+            sendDialogflow(e){
+                console.log(this.response, e)
             }
         },
         mounted() {
-            if(this.fluid){
-                this.scrollMessagesDown();
-            }
+            // config dotenv
+            this.AI_SESSION_ID = uuidv1();
+
+            console.log(this.ACCESS_TOKEN, this.AI_SESSION_ID)
         }
     }
 </script>
@@ -52,9 +58,11 @@
         font-family: 'Millimetre', Arial, sans-serif;
 
         &__form{
-            display: block;
+            display: flex;
             box-sizing: border-box;
             width: 100%;
+            border-radius: 5px;
+            background: #444;
 
         }
 
@@ -62,14 +70,20 @@
             display: inline-block;
             box-sizing: border-box;
             width: 100%;
-            background: #444;
             border: none;
+            background: none;
             font-size: 1rem;
             font-family: 'Millimetre', Arial, sans-serif;
             padding: 15px 15px;
             color: white;
-            border-radius: 5px;
             outline: none;
+        }
+
+        &__submit{
+            color: white;
+            font-family: 'Millimetre', Arial, sans-serif;
+            font-size: 0.8rem;
+            margin-left: 5px;
         }
 
     }
