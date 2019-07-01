a23473b6571fa6fc1cc132a8b634955c53859c4f
Robin Simonklein <rsimonklein@gmail.com>
2019-07-01 09:37:18 +0200

    Add unit tests

--- tests/unit/scenario.spec.js
+++ tests/unit/scenario.spec.js
@@ -11,22 +11,74 @@ for(let [key] of Object.entries(scenario)){
   keysArray.push(key);
 }
 
+
 // Test que les éléments du scénario ne contiennent pas plusieurs fois le même ID
 describe('Duplication d\'IDs dans le scénario', () => {
   it('ne doit pas avoir de doubles', () => {
     expect (hasDuplicates(keysArray)).toBe(false);
   });
 });
-/*
+
+
 // Test que tous les atributs next des noeuds du scénario mènent à un noeud existant
-describe('Duplication d\'IDs dans le scénario', () => {
-  it('ne doit pas avoir de doubles', () => {
-    const keysArray = [];
-    for(let [key] of Object.entries(scenario)){
-      keysArray.push(key);
+describe('Tous les noeuds peuvent s\'enchaîner', () => {
+  it('leur ID suivant doit exister', () => {
+    const unreachables = [];
+
+    for(let [key, value] of Object.entries(scenario)){
+      switch(value.component.toLowerCase()) {
+        case 'end':
+          break;
+        case 'usercardscontainer':
+        case 'user-cards-container':
+        case 'popup':
+        case 'colorpalette':
+        case 'color-palette':
+          if(value.data.choices){
+            value.data.choices.forEach((choice) => {
+              if (!keysArray.includes(choice.next)) {
+                unreachables.push([key, choice.next]);
+              }
+            });
+          }else{
+            unreachables.push(key);
+          }
+          break;
+        case 'canvasdraw':
+        case 'canvas-draw':
+          if(value.data.next){
+            for(let [nextKey, nextValue] of Object.entries(value.data.next)){
+              if(!keysArray.includes(nextValue)){
+                unreachables.push([key, nextKey, nextValue])
+              }
+            }
+          }else{
+            unreachables.push(key);
+          }
+          break;
+        default:
+          if(value.next){
+            if(value.next.priority){
+              if (!keysArray.includes(value.next.priority)) {
+                unreachables.push([key, value.next.priority]);
+              }
+              if (!keysArray.includes(value.next.default)) {
+                unreachables.push([key, value.next.default]);
+              }
+            }else {
+              if (!keysArray.includes(value.next)) {
+                unreachables.push(key);
+                break;
+              }
+            }
+          }else{
+            unreachables.push(key);
+            break;
+          }
+          break;
+      }
     }
-    expect (hasDuplicates(keysArray)).toBe(false);
+
+    expect(unreachables).toEqual([]);
   });
 });
-
- */
