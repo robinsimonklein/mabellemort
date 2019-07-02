d80fdf9ae8e73810adfe6d733ca146a0ab44ec0e
Robin Simonklein <rsimonklein@gmail.com>
2019-07-02 04:32:47 +0200

    WIP // Intro view

--- src/views/Intro.vue
+++ src/views/Intro.vue
@@ -2,13 +2,33 @@
     <div class="intro view">
         <h1 class="intro__title">Ma Belle Mort</h1>
         <h2 class="intro__subtitle">Contacts</h2>
-        <ul class="intro__list">
-            <h3 class="intro__list-title">En ligne</h3>
-            <li class="intro__list-item">
-                <img class="user__profile" src="/img/profile/death_profile.png"/>
-                <span class="user__name">La Mort</span>
-            </li>
-        </ul>
+        <div class="intro__contacts">
+            <ul class="intro__list">
+                <h3 class="intro__list-title">En ligne</h3>
+                <li class="intro__list-item">
+                    <div class="user__profile user__profile--active" style="background-image: url(/img/profile/death_profile.png)"></div>
+                    <span class="user__name">La Mort</span>
+                </li>
+            </ul>
+            <ul class="intro__list">
+                <h3 class="intro__list-title">Inactif</h3>
+                <li class="intro__list-item">
+                    <div class="user__profile user__profile--active" style="background-image: url(/img/profile/truth_profile.png)"></div>
+                    <span class="user__name">La Vérité</span>
+                </li>
+                <li class="intro__list-item">
+                    <div class="user__profile user__profile--active" style="background-image: url(/img/profile/arte_profile.png)"></div>
+                    <span class="user__name">Arte</span>
+                </li>
+            </ul>
+            <ul class="intro__list">
+                <h3 class="intro__list-title">Hors ligne</h3>
+                <li class="intro__list-item">
+                    <div class="user__profile user__profile--active" style="background-image: url(/img/profile/life_profile.png)"></div>
+                    <span class="user__name">La Vie</span>
+                </li>
+            </ul>
+        </div>
     </div>
 </template>
 
@@ -32,5 +52,58 @@
         text-transform: uppercase;
         font-family: 'Daubenton', serif;
     }
+
+    &__contacts {
+        padding: 0 2rem;
+    }
+
+    &__list{
+        list-style-type: none;
+        padding: 0;
+    }
+    &__list-item{
+        display: flex;
+        align-items: center;
+    }
+
+    .user__profile {
+
+        position: relative;
+        height: 40px;
+        width: 40px;
+        background-size: cover;
+        background-repeat: no-repeat;
+
+        &:after {
+            content: '';
+            position: absolute;
+            bottom: 0;
+            right: 0;
+            height: 10px;
+            width: 10px;
+            border-radius: 100%;
+
+        }
+
+        &--active{
+            &:after {
+                background-color: #6BD37B;
+            }
+        }
+        &--inactive{
+            &:after {
+                background-color: #6BD37B;
+            }
+        }
+        &--offline{
+            &:after {
+                background-color: #6BD37B;
+            }
+        }
+    }
+    .user__name {
+        margin-left: 0.9rem;
+    }
+
 }
 </style>
